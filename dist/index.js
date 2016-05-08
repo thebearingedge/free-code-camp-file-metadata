946d6ac65a2798019046a0288c82a7befe3408e4
Tim Davis <thebearingedge@gmail.com>
2016-05-07 20:13:21 -0700

    add landing page

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,21 @@
 'use strict';
 
+var _path = require('path');
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
+
+var _jade = require('jade');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var port = process.env.PORT || 3000;
 
+var indexHtml = (0, _jade.renderFile)((0, _path.join)(__dirname, '/index.jade'));
+
 (0, _express2.default)().get('/', function (req, res) {
-  return res.send('hello app');
+  return res.send(indexHtml);
 }).listen(port, function (_) {
   return console.log('listening on ' + port);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
+import { join } from 'path'
 import express from 'express'
-
+import { renderFile } from 'jade'
 
 const port = process.env.PORT || 3000
 
+const indexHtml = renderFile(join(__dirname, '/index.jade'))
 
 express()
-  .get('/', (req, res) => res.send('hello app'))
+  .get('/', (req, res) => res.send(indexHtml))
   .listen(port, _ => console.log(`listening on ${port}`))
